8256b0c09ac7fed91c6fb9ea59c333f3625107a1
Jay prakash kumar <kumarjp457@gmail.com>
2025-09-25 11:21:13 +0530

    Add files via upload
    basic of cpp

---
+++ basics/firstprogram.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+// using namespace std;
+// int yourname=5;
+// int main() {
+//     cout << yourname;
+//     return 0;
+// }
+// pre and post increment 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x=6;
+//     ++x;// pre increment
+//     cout<<x<<endl;
+//     x++;// post increment 
+//     cout<<x;
+// }
+
+
---
+++ basics/problem2.cpp
@@ -0,0 +1,29 @@
+//wap to find remainder when a is divided by b such that a is greater than b
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a;
+//     cin>>a;
+//     int b;
+//     cin>>b;
+//     int c;
+//     c=a/b;
+//     int r;
+//     r=a-b*c;
+//     cout<<r;
+// }
+
+// wap to calculate percentage of 5 subject 
+#include<iostream>
+using namespace std;
+int main()
+{
+float math=50;
+float physics=80;
+float art=75;
+float chemistry=85;
+float biology=90;
+float result= ((math+physics+art+chemistry+biology)/500)*100;
+cout<<result;
+}
