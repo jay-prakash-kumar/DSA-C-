9d976e5636392dd0c4a03011e2d8676b39f29d75
Jay prakash kumar <kumarjp457@gmail.com>
2025-09-27 20:31:01 +0530

    loops including some concept that you really like

---
+++ loops/forloop.cpp
@@ -0,0 +1,38 @@
+//  wap to  print table of 19
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a=19;
+//     for(int i=1;i<=10;i++)
+//     {
+//         cout<<a*i<<endl;
+//     }
+// }
+
+// diplay the ap  100 97 94 upto all positive term
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+
+// for(int i=100;i>0;i=i-3)
+// {
+//     cout<<i<<" ";
+// }
+// }
+
+ //print all alphabet (uppercase) with thier ascii value
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+    int a;
+    
+    for(int a=65;a<=90;a++)
+    {
+        cout<<a<<" "<<(char)a<<endl;
+    }
+ }
+
+
