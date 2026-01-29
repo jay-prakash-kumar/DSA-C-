b5fe94782adb24d2ceeb50d48490775356c9d4f8
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-01-30 00:07:38 +0530

    functions

---
+++ Function/2.cpp
@@ -0,0 +1,55 @@
+// print minimum of two number
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<min(a,b);// by using min function
+// }
+ 
+// by making function other than main
+// #include<iostream>
+// using namespace std;
+// int mini(int c,int d)
+// {
+//     int e;
+//     if(c<d){
+//         e=c;
+//     }
+//   else 
+//   {
+//     e=d;
+//   }
+//  return e;
+// }
+// int main()
+// {
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<mini(a,b);
+// }
+// sqrt
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+// int mini(int c,int d)
+// {
+//     int e;
+//     if(c<d){
+//         e=c;
+//     }
+//   else 
+//   {
+//     e=d;
+//   }
+//  return e;
+// }
+int main()
+{
+    // int a,b;
+    // cin>>a;
+    cout<<sqrt(25);
+}
+
