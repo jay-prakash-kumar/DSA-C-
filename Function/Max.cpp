d7a85ac66d190ebda35cc9e8d6a34202132cb4b0
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-05 15:41:54 +0530

    Max no

---
+++ Function/Max.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int maxfun(int a,int b,int c)
+{
+    if(a>b&&a>c)
+    {
+        return a;
+    }
+    else if(b>a&&b>c)
+    {
+        return b;
+    }
+    else{
+        return c;
+    }
+
+}
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout<<maxfun(a,b,c);
+}
