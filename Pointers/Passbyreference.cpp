1537e6a944142e71984df3908e0d802d937bec2c
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-07 22:34:11 +0530

    swap by using pass by reference

---
+++ Pointers/Passbyreference.cpp
@@ -0,0 +1,24 @@
+// //swap two number by passing address(pass by refernce)
+// #include<iostream>
+// using namespace std;
+// void swap(int *x,int *y)
+// {
+//     int temp=*x;
+//     *x = *y;
+//     *y =temp;
+//     return;
+    
+// }
+// int main()
+// {
+//     int a=5;
+//     int b=6;
+//     // swap(&a,&b);
+//     int *x=&a;
+//     int *y=&b;
+//     swap(x,y);
+//     cout<<a<<" "<<b;
+    
+
+
+// }
