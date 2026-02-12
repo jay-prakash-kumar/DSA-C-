8ea508a6db36f01f959c7435d648699093e0b58f
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-12 09:21:19 +0530

    adding two pointer and various operation

---
+++ Pointers/PointerArithmetic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int x =4;
+    // int *ptr =&x;
+    // cout<<*ptr<<endl;//4
+    // ptr=ptr+1;
+    // cout<<*ptr<<endl;
+
+//     int x =4;
+//     int *ptr =&x;
+//     cout<<*ptr<<endl;//4
+//     *ptr=*ptr+1;
+//     (*ptr)++;// or increment by this also 
+//     cout<<*ptr<<endl;
+}
