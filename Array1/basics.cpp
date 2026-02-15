659980c8df48227d77650c55dba3116690f69a45
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-15 22:53:32 +0530

    basics of array

---
+++ Array1/basics.cpp
@@ -0,0 +1,72 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     int sum =0;
+//    for(int i=0;i<n;i++)
+//     {
+//         cin>>arr[i];
+//     }
+    
+//    for(int i=0;i<n;i++)
+//    {
+//     sum=sum+arr[i];
+
+//     }
+//     cout<<sum;
+// }
+//product 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int product=1;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     for(int i=0;i<n;i++)
+//     {
+//         product=product*arr[i];
+//     }
+//     cout<<product;
+// }
+
+// 12345
+// 3 -found 
+// 123 
+// 4-notfound
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+
+//     int n;
+//     cin>>n;
+//     int key;
+//     cin>>key;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         cin>>arr[i];
+
+//     }
+//     for(int i=0;i<n;i++)
+//     {
+//         if(arr[i]=key)
+//         {
+//             cout<<"found";
+//             break;
+//         }
+//         else{
+//             cout<<"not found";
+//         }
+//     }
+// }
+
