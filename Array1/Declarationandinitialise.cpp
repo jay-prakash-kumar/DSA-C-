b6d86904db31265b717ef048723f73031e8cfbe6
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-16 09:40:15 +0530

    declaration and initialisation of array

---
+++ Array1/Declarationandinitialise.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={10,20,30,40,50};// here 5 is size of array;
+    cout<<arr[2];// here 2 is index inside the square bracket.it is always 1 less than the size of an array;
+
+}
