72cf305911505ff4359c5dec6c9a6ac085aa943b
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-17 09:36:02 +0530

    search given element

---
+++ Array1/Linearsearch.cpp
@@ -0,0 +1,34 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;// size of array ;
+//     cin>>n;
+//     int arr[n];
+//     int key;
+//     cin>>key;
+    
+    
+//     //input
+//     for(int i=0;i<n;i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     bool flag =false;
+//     for(int i=0;i<n;i++)
+//     {
+//         if(arr[i]==key)
+//         {
+            
+//             flag =true;
+//             break;
+//         }
+//     }
+//     if(flag==true)
+//     {
+//         cout<<"element found";
+//     }
+//     else{
+//         cout<<"element not found";
+//     }
+// }
