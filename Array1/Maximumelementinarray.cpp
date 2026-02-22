67403af714facbbd7d88909f22e586ed247d3301
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-02-22 22:47:45 +0530

    maximum element in array

---
+++ Array1/Maximumelementinarray.cpp
@@ -0,0 +1,22 @@
+// Find the maximum value out pf all the elements in the array.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    cout<<max;
+}
