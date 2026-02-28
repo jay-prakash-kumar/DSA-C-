42f92d8098126815158dee8d7ea4b104b320bad7
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-03-01 01:41:03 +0530

    passing array to function

---
+++ Array2/Passingarraytofunction.cpp
@@ -0,0 +1,45 @@
+// #include<iostream>
+// using namespace std;
+// void display(int a[])// basically this a is different from arr
+// {
+// for(int i=0;i<=4;i++)
+// {
+//     cout<<a[i]<<" ";
+// }
+
+// }
+// void change(int b[])
+// {
+//     b[0]=120;
+// }
+// int main()
+// {
+//     int arr[5]={1,2,3,4,5};
+//     // display(arr);
+//     change(arr);
+//     display(arr);
+// }
+
+//passing array as well size of an array
+// #include<iostream>
+// using namespace std;
+// void display(int a[],int size)// basically this a is different from arr
+// {
+// for(int i=0;i<=4;i++)
+// {
+//     cout<<a[i]<<" ";
+// }
+
+// }
+// void change(int b[],int size)
+// {
+//     b[0]=120;
+// }
+// int main()
+// {
+//     int arr[5]={1,2,3,4,5};
+//     // display(arr)
+//     int size=sizeof(arr)/sizeof(arr[0]);
+//     change(arr,size);
+//     display(arr,size);
+// }
