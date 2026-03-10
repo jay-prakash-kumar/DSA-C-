8afab4b9ad2325522bfbc33b4ac4074900beb358
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-03-10 22:22:30 +0530

    basic

--- Function/1.cpp
+++ Function/1.cpp
@@ -1,18 +1,46 @@
 #include<iostream>
 using namespace std;
-int startriangle(int n)
-{
+// int startriangle(int n)
+// {
     
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=i;j++)
-        {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//         {
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main()
+// {
+//     startriangle(4);
+// }
+
+// pass by value 
+// int triangle(int x)
+// {   x=x+2;
+//     return x;
+// }
+
+// int main()
+// {
+//     int a=10;
+//     int b=triangle(a);// this is only taking copy of a and it does not chnage the valuer of a;
+//     cout<<b;
+    
+// }
+// pass by refernce 
+int triangle(int &a)
+{
+        a=a+12;
+        return a;
 }
+
 int main()
 {
-    startriangle(4);
+    int a=10;
+    triangle(a);
+    cout<<a;
+
 }
