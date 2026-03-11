c32e55852a372f1ad39177a8f8724057752c94e2
Jay Prakash Kumar <kumarjp457@gmail.com>
2026-03-11 10:32:44 +0530

    combination

---
+++ Function/Combination.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+// using namespace std;
+//  int fact(int n)
+//     {
+//         int fact=1;
+//         for(int i=2;i<=n;i++)
+//         {
+//             fact=fact*i;
+//         }
+//         return fact;
+//     }
+//     int combination(int n,int r)
+//     {
+//         int ncr=fact(n)/(fact(r)*fact(n-r));
+//         return ncr;
+//     }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     for(int i=0;i<=n;i++)
+//     {  
+//          for(int j=0;j<=i;j++)
+//          {
+//              int ncr;
+//                ncr=combination(i,j);
+//                cout<<ncr;
+//          }
+//          cout<<endl;
+         
+//     }
+
+// }
